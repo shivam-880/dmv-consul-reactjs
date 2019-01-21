a3ad00e710fb1cce20b361a7594946e54b46d1e4
codingkapoor <mailtoshivamk@gmail.com>
2019-01-21 13:51:55 +0530

    ref

--- src/app/App.js
+++ src/app/App.js
@@ -7,24 +7,24 @@ import ViewSelectorContainer from '../view-selector/ViewSelectorContainer';
 import SearchBoxContainer from '../search-box/SearchBoxContainer';
 
 import '../style.css';
-import { ViewSelectorColumnWrapper, HeaderRow } from './StyledComponent';
+import { Wrapper, Header } from './StyledComponent';
 
 const App = () => {
   return (
-    <div className="ui container">
-      <div className="ui three column grid">
-        <HeaderRow>
+    <div className='ui container'>
+      <div className='ui three column grid'>
+        <Header className='row'>
           <ExpandCollapseAllContainer />
-          <div className="column">
+          <div className='column'>
             <SearchBoxContainer />
           </div>
-        </HeaderRow>
-        <div className="row">
-          <ViewSelectorColumnWrapper>
+        </Header>
+        <div className='row'>
+          <Wrapper className= 'two wide column'>
             <ViewSelectorContainer />
-          </ViewSelectorColumnWrapper>
+          </Wrapper>
           <DeploymentModelContainer />
-          <div className="column">
+          <div className='column'>
             <DetailsContainer />
           </div>
         </div>
--- src/app/StyledComponent.js
+++ src/app/StyledComponent.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 
-export const HeaderRow = styled.div.attrs(props => ({
-    className: 'row'
-}))`
+export const Header = styled.div`
     display: flex !important;
     align-items: center !important;
     justify-content: center !important;
@@ -10,9 +8,7 @@ export const HeaderRow = styled.div.attrs(props => ({
     margin-top: 30px;
 `;
 
-export const ViewSelectorColumnWrapper = styled.div.attrs(props => ({
-    className: 'two wide column'
-}))`
+export const Wrapper = styled.div`
     display: flex !important;
     justify-content: space-evenly !important;
     flex-direction: column !important;
