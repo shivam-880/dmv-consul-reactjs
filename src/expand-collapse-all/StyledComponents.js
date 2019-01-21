c085298107c9ba858f0ebb30965ea64712126268
codingkapoor <mailtoshivamk@gmail.com>
2019-01-21 12:30:04 +0530

    ref expandcollapse styledcomponent

--- src/expand-collapse-all/StyledComponents.js
+++ src/expand-collapse-all/StyledComponents.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 
-export const ExpandAll = styled.i.attrs({
-    className: 'fa fa-plus-square fa-lg'
-})`
+const buttonStyle = `
     padding: 5px;
     color: #5d5c5c;
     font-size: 1.4em;
@@ -11,13 +9,10 @@ export const ExpandAll = styled.i.attrs({
     }
 `;
 
+export const ExpandAll = styled.i.attrs({
+    className: 'fa fa-plus-square fa-lg'
+})`${buttonStyle}`;
+
 export const CollapseAll = styled.i.attrs({
     className: 'fa fa-minus-square fa-lg'
-})`
-    padding: 5px;
-    color: #5d5c5c;
-    font-size: 1.4em;
-    &:hover{
-        color: #D9D9D9;
-    }
-`;
+})`${buttonStyle}`;
